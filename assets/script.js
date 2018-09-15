e5a335b77f6c31e23bd4cd491927e1f758ebc8a4
Jiri Trapl <jiri.trapl@gmail.com>
2018-09-15 14:03:03 +0200

    initial

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+function getQuery() {
+  var query = $('#query').val();
+  getData(query);
+}
+
+function getData(query) {
+  var url = 'https://en.wikipedia.org/w/api.php?'
+    + 'action=query&list=search&format=json&srprop=snippet'
+    + '&srsearch=' + query + '&callback=?';
+
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function(json) {
+      listEntries(json);
+    },
+    error: function(e) {
+      console.log(e.message);
+    }
+  });
+}
+
+function listEntries(data) {
+  var entries = '';
+  for(var i = 0; i < data.query.search.length; i++) {
+    entries += createEntry(data.query.search[i]);
+  }
+  $('#page').html(entries);
+}
+
+function createEntry(entry) {
+  var wikiBase = 'http://wikipedia.org/wiki/';
+  var titleLink = '<a href="'
+                  + wikiBase
+                  + entry.title
+                  + '">'
+                  + entry.title
+                  + '</a>';
+  var html = '<div class="entry"><div><span class="title">'
+              + titleLink
+              + '</span><br><br><span'
+              + entry.snippet
+              + '</span></div></div>';
+  return html;
+}
